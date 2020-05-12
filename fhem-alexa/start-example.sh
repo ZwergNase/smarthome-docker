de188b93d97d8ccca06d346d32ae50ec10b5a28a
ZwergNase <oliver.friesen@gmail.com>
2020-05-12 21:26:41 +0200

    slight adjustments

--- fhem-alexa/start-example.sh
+++ fhem-alexa/start-example.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo docker run --detach  --restart=always --name fhem-of --mount source=fhem_data,target=/home/fhem --mount source=weewx_share,target=/var/www/weewx --device /dev/ttyUSB0:/dev/ttyUSB0 --publish 8083-8085:8083-8085 fhem-of:arm32v7
+sudo docker run --detach  --restart=always --name fhem-alexa --mount source=fhem_data,target=/home/fhem --mount source=weewx_share,target=/var/www/weewx --device /dev/ttyUSB0:/dev/ttyUSB0 --publish 8083-8085:8083-8085 oliverf/fhem-alexa:latest
 
-# --detach                                      im Hintergrund ausführen / run in background
-# --restart=always                              Container automatisch (neu)starten / (re)start container automaticly 
-# --name=fhem                                   Container "fhem" nennen / name container "fhem"
-# --mount souce=fhem_data,target=/home/fhem     /home/fhem im Volume fhem_data sichern / save /home/fhem in volume fhem_data 
-# --publish 8083-8085:8083-8085                 Ports 8083-8085 zugänglich machen / allow access on port 8083-8085
-# oliverf/fhem:latest                           Image / name of the image
+# --detach                                      	im Hintergrund ausführen / run in background
+# --restart=always                              	Container automatisch (neu)starten / (re)start container automaticly
+# --name=fhem-alexa                             	Container "fhem-alexa" nennen / name container "fhem-alexa"
+# --mount souce=fhem_data,target=/home/fhem     	/home/fhem im Volume fhem_data sichern / save /home/fhem in volume fhem_data
+# --mount source=weewx_share,target=/var/www/weewx	daten von weewx einbinden / make data from weeex accessible
+# --publish 8083-8085:8083-8085	        	        Ports 8083-8085 zugänglich machen / allow access on port 8083-8085
+# oliverf/fhem-alexa:latest     	                Image / name of the image
