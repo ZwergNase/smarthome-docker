4b181a9d93269993b49fc50e1f11c1fe762b08c0
ZwergNase <oliver.friesen@gmail.com>
2021-03-08 20:53:26 +0100

    removed weewx-share, changed to weewx-mqtt

--- fhem-of/start-example.sh
+++ fhem-of/start-example.sh
@@ -2,12 +2,11 @@
 
 sudo docker run --detach  --restart=always --name fhem-of --mount source=fhem_data,target=/home/fhem --publish 8083-8085:8083-8085 --publish 2121:2121 --publish 1883:1883 oliverf/fhem-of:latest
 
-# --detach                                      	im Hintergrund ausführen / run in background
-# --restart=always                              	Container automatisch (neu)starten / (re)start container automaticly
-# --name=fhem-of	                             	Container "fhem-of" nennen / name container "fhem-of"
-# --mount souce=fhem_data,target=/home/fhem     	/home/fhem im Volume fhem_data sichern / save /home/fhem in volume fhem_data
-# --mount source=weewx_share,target=/var/www/weewx	daten von weewx einbinden / make data from weeex accessible
-# --publish 8083-8085:8083-8085	        	        Ports 8083-8085 zugänglich machen / allow access on port 8083-8085
+# --detach						im Hintergrund ausführen / run in background
+# --restart=always					Container automatisch (neu)starten / (re)start container automaticly
+# --name=fhem-of					Container "fhem-of" nennen / name container "fhem-of"
+# --mount souce=fhem_data,target=/home/fhem		/home/fhem im Volume fhem_data sichern / save /home/fhem in volume fhem_data
+# --publish 8083-8085:8083-8085				Ports 8083-8085 zugänglich machen / allow access on port 8083-8085
 # --publish 2121:2121					Port für smartvisu / port for smartvisu
 # --publish 1883:1883					Port für mqtt / port for mqtt
-# oliverf/fhem-of:latest	     	                Image / name of the image
+# oliverf/fhem-of:latest				Image / name of the image
