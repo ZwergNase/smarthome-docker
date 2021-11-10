a02d189e3fa1db191edffd8449210ff2ffe628be
ZwergNase <oliver.friesen@gmail.com>
2021-11-10 21:03:35 +0100

    use :latest

--- nginx-php_fpm/start-example.sh
+++ nginx-php_fpm/start-example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo docker run  --detach  --restart=always --name nginx-php --mount source=smartvisu_data,target=/var/www/html/smartvisu --mount source=icecoder_data,target=/var/www/html/ICEcoder -p 80:8080 oliverf/nginx-php_fpm-amd64 
+sudo docker run  --detach  --restart=always --name nginx-php --mount source=smartvisu_data,target=/var/www/html/smartvisu --mount source=icecoder_data,target=/var/www/html/ICEcoder -p 80:8080 oliverf/nginx-php_fpm:latest
